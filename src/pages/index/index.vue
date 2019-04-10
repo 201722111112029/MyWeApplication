21cd4c0ece65741d303b97ad1adec2210722bfcc
student <student@hubu.edu.cn>
2019-04-10 11:36:21 +0800

    add cloud database information

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -43,59 +43,15 @@
         <i-grid-label>历史</i-grid-label>
     </i-grid-item>
 </i-grid>
-<i-grid i-class="no-border">
-    <i-grid-item i-class="no-border">
-        <i-grid-icon>
-            <image src="../../static/images/bookThree.png" />
-        </i-grid-icon>
-        <i-grid-label>诗歌</i-grid-label>
-    </i-grid-item>
-        <i-grid-item i-class="no-border">
-        <i-grid-icon>
-            <image src="../../static/images/bookThree.png" />
-        </i-grid-icon>
-        <i-grid-label>散文</i-grid-label>
-    </i-grid-item>
-        <i-grid-item i-class="no-border">
-        <i-grid-icon>
-            <image src="../../static/images/bookThree.png" />
-        </i-grid-icon>
-        <i-grid-label>解压</i-grid-label>
-    </i-grid-item>
-</i-grid>
-
-<i-panel title="书单推荐"> 
 
 <view v-for="bookList in bookList" :key='bookList'>
-<i-card i-class="top-padding" :title="bookList.introduction" :extra="bookList.value" thumb="cloud://book-a8926e.626f-book-a8926e/Chrysanthemum.jpg">
+<i-card i-class="top-padding" :title="bookList.introduction" :extra="bookList.value" :thumb="bookList.webAddress">
     <view slot="content" >{{bookList.name}}</view>
     <view slot="footer">{{bookList.type}}</view>
 </i-card>
 </view>
 
 
-
-<i-card i-class="top-padding" title="卡片标题" extra="额外内容" thumb="https://images.pexels.com/photos/46274/pexels-photo-46274.jpeg?cs=srgb&dl=blur-blurred-book-46274.jpg&fm=jpg">
-    <view slot="content">为什么要读点哲学？这本书帮你摸到哲学的门。</view>
-    <view slot="footer">入门级哲学经典</view>
-</i-card>
-<i-card i-class="top-padding" title="卡片标题" extra="额外内容" thumb="https://images.pexels.com/photos/1166657/pexels-photo-1166657.jpeg?cs=srgb&dl=book-bookcase-books-1166657.jpg&fm=jpg">
-    <view slot="content">曾经落魄潦倒，后来名扬天下的文学大师。</view>
-    <view slot="footer">文学家传记</view>
-</i-card>
-<i-card i-class="top-padding" title="卡片标题" extra="额外内容" thumb="https://images.pexels.com/photos/24576/pexels-photo.jpg?cs=srgb&dl=antique-book-encyclopedia-24576.jpg&fm=jpg">
-    <view slot="content">大师的巅峰之作，好看的让你灵魂震颤！</view>
-    <view slot="footer">精神养料</view>
-</i-card>
-<i-card i-class="top-padding" title="卡片标题" extra="额外内容" thumb="https://images.pexels.com/photos/545049/pexels-photo-545049.jpeg?cs=srgb&dl=blanket-bloom-blooming-545049.jpg&fm=jpg">
-    <view slot="content">含金量超高的名家随笔，让你与自己的灵魂相遇。</view>
-    <view slot="footer">随笔</view>
-</i-card>
-<i-card i-class="top-padding" title="卡片标题" extra="额外内容" thumb="https://images.pexels.com/photos/1765033/pexels-photo-1765033.jpeg?cs=srgb&dl=art-blank-blue-1765033.jpg&fm=jpg">
-    <view slot="content">点燃对生活的熊熊热情。</view>
-    <view slot="footer">诗歌</view>
-</i-card>
-</i-panel > 
   </div>
 </template>
 
@@ -126,11 +82,15 @@ export default {
           env: 'book-a8926e' })
     db.collection('books').get().then(
       res => {
-        console.log(res.data)
+       console.log(res.data)
         this.bookList = res.data
       }
     )
+
     // let app = getApp()
+     wx.cloud.callFunction({ name: 'zheng' }).then(
+      res => {console.log(res)}
+)
   }
 }
 </script>
