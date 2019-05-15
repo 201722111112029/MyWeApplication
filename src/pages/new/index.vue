4c5429e61b15111a067016bd562156b6b8e3a4d4
201722111112029 <2696191050@qq.com>
2019-05-15 17:09:48 +0800

    connect the database and can add information

--- src/pages/new/index.vue
+++ src/pages/new/index.vue
@@ -1,24 +1,28 @@
 <template>
   <div>
-           <i-input :value="name" title="书名" autofocus placeholder="名称" />
-           <i-input :value="introduction" title="关于本书" placeholder="介绍" />
-           <i-input :value="type" title="作品类型" placeholder="类型" />
-           <i-input :value="price" title="作品价格" placeholder="价格" />
+    <i-panel title="我的分享">
+           <i-input :value="name" @change="changeName($event)" title="书名" autofocus placeholder="名称" maxlength="20"/>
+           <i-input :value="introduction" @change="changeIntroduction($event)" title="关于本书" placeholder="介绍"  maxlength="50"/>
+           <i-input :value="type" @change="changeType($event)" title="作品类型" placeholder="类型"  maxlength="20"/>
+           <i-input :value="value" @change="changeValue($event)" title="推荐价格" placeholder="价格" maxlength="20" />
+           <i-button @click="handleClick()">提交分享</i-button>
+           <i-toast id="toast" />
+    </i-panel >
   </div>
 </template>
 
 <script>
+const { $Toast } = require('../../../static/dist/base/index');
 import card from '@/components/card'
 
 export default {
   data () {
     return {
-      name:"",
-      introduction:"",
-      type:"",
-      price:""
-    }
-    
+      name: "",
+      introduction: "",
+      type: "",
+      value: ""
+    }  
   },
 
   components: {
@@ -26,6 +30,47 @@ export default {
   },
   
   methods: {
+      changeName(event){
+      console.log(event)
+      this.name = event.mp.detail.detail.value
+    },
+      changeIntroduction(event){
+      console.log(event)
+      this.introduction = event.mp.detail.detail.value
+    },
+      changeType(event){
+      console.log(event)
+      this.type = event.mp.detail.detail.value
+    },
+      changeValue(event){
+      console.log(event)
+      this.value = event.mp.detail.detail.value
+    },
+    handleClick(){
+      if(this.name && this.introduction && this.type && this.value){
+        let event = {
+          name: this.name,
+          introduction: this.introduction,
+          type: this.type,
+          value: this.value,
+          webAddress:'cloud://book-a8926e.626f-book-a8926e/red&black.jpg' 
+        }
+              wx.cloud.callFunction({name:'new_book', data: event}).then(
+                res => {
+                  console.log(res)
+                }
+              )
+        $Toast({
+            content: '分享成功',
+            type: 'success'
+        });
+       }else{
+       $Toast({
+            content: '分享失败(请填入相关信息)',
+            type: 'warning'
+        });        
+      }
+    }
   },
 
   created () {
